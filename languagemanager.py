708cd80da64e9f162d7997b25545103536cf75c1
5487th <adamasteway268th@gmail.com>
2026-04-19 19:35:17 +0300

    create language manager and assets folder contatining translations json file

---
+++ languagemanager.py
@@ -0,0 +1,44 @@
+import pathlib
+import json
+import warnings
+
+class LanguageManager:
+    ENGLISH_LANGUAGE_KEY = "en"
+    AMHARIC_LANGUAGE_KEY = "am"
+    TRANSLATIONS_FILE_PATH = pathlib.Path(__file__).parent/'assets'/'translations.json'
+    TRANSLATIONS_DICT = {}
+
+    def __init__(self, default_lang="en"):
+        self.current_lang=""
+
+        if default_lang in [LanguageManager.ENGLISH_LANGUAGE_KEY, LanguageManager.AMHARIC_LANGUAGE_KEY]:
+            self.current_lang=default_lang
+        
+        self.registered_widgets = []
+        with open(self.TRANSLATIONS_FILE_PATH,"r") as file:
+         LanguageManager.TRANSLATIONS_DICT = json.load(file)
+        
+    def set_language(self, lang):
+        if lang in [LanguageManager.ENGLISH_LANGUAGE_KEY, LanguageManager.AMHARIC_LANGUAGE_KEY]:
+            self.current_lang=lang
+            self.update_all_widgets()
+        else:
+            warnings.warn("attempted to set language with invalid language, language set to english")
+            self.current_lang = self.ENGLISH_LANGUAGE_KEY
+           
+    def register_widget(self, widget, key):
+        self.registered_widgets.append((widget, key))
+        widget.configure(text=self.translate(key))
+    
+    def update_all_widgets(self):
+        for widget, key in self.registered_widgets:
+            widget.configure(text=self.translate(key))
+    
+    def translate(self, key):
+        if self.TRANSLATIONS_DICT:
+            translation = self.TRANSLATIONS_DICT.get(key,{}).get(self.current_lang)
+            if translation:
+                return translation
+            else:
+                warnings.warn("key not found, returning passed key")
+                return key
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import customtkinter as ctk
+import os
 from blinker import signal
+
 from usermanager import UserManager,User
+from languagemanager import LanguageManager
+
+appdata_local = os.getenv('LOCALAPPDATA')
+save_dir = os.path.join(appdata_local, "MyApp")
+
 
 class Menu:
     def open_menu(self, root):
@@ -9,13 +16,34 @@ class Menu:
     def close_menu(self, root):
         pass
 
+
+    
 root = ctk.CTk()
 
-Current_open_menu:Menu = None
+class user_login_menu(Menu):
+    def __init__(self):
+        self.on_language_dropdown_value_changed = signal("on_language_dropdown_value_changed")
+    def open_menu(self, root):
+        self.languages = ["English", "አማርኛ"]
+        self.language_dropdown = ctk.CTkOptionMenu(root,values=self.languages,)
+        self.language_dropdown.pack(side = "top", anchor = "nw", pady = 20, padx = 20)
+
+        self.choose_a_user_label = ctk.CTkLabel(root, text= "welcome, login or create a user",font=("Roboto",40))
+        self.choose_a_user_label.pack(pady=40)
+    
+    def close_menu(self, root):
+        return super().close_menu(root)
+
+
+Current_open_menu:Menu = user_login_menu()
 User_Manager:UserManager = UserManager()
+language_manager:LanguageManager = LanguageManager(LanguageManager.AMHARIC_LANGUAGE_KEY)
+
+print(language_manager.translate("welcome, login or create a user"))
 
 if Current_open_menu is not None:
     Current_open_menu.open_menu(root)
 
 root.geometry("1000x600")
+ctk.set_appearance_mode("light")
 root.mainloop()
