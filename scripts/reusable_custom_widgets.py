20a1586f67db931dadbc590a557bac84b7e7ef9c
5487th <adamasteway268th@gmail.com>
2026-05-01 13:22:43 +0300

    -make user login icon display user profiels -add us profile and user name to user login menu

--- scripts/reusable_custom_widgets.py
+++ scripts/reusable_custom_widgets.py
@@ -1,13 +1,15 @@
 from customtkinter import *
 import warnings
 from blinker import signal
+from PIL import Image
+import pathlib
 
 from scripts.user_manager import User
 from scripts.user_manager import UserManager
 from scripts.language_manager import LanguageManager
 
 class UserLoginIcon(CTkFrame):
-    def __init__(self, master, user:User, **kwargs):
+    def __init__(self, master, user_manager:UserManager, user:User, **kwargs):
         super().__init__(master, **kwargs)
         if not user:
             warnings.warn("attempted to create user login icon with no user, user icon not created")
@@ -16,6 +18,7 @@ class UserLoginIcon(CTkFrame):
         self.user = user
         self.master = master
         self.on_user_pressed_icon = signal(f"on_user_pressed_{self.user.username}_icon")
+        self.user_manager = user_manager
 
         self.configure(width=150,height=190,corner_radius=10)
         self.pack_propagate(False)
@@ -23,6 +26,20 @@ class UserLoginIcon(CTkFrame):
         self.bind("<Enter>",self.on_mouse_entered)
         self.bind("<Leave>", self.on_mouse_exit)
         self.bind("<Button-1>", self.on_mouse_clicked)
+
+        if pathlib.Path(user.profile_picture_path).exists():
+            image_object = Image.open(user.profile_picture_path)
+            ctk_image=CTkImage(light_image=image_object, dark_image=image_object, size=(120, 120)) 
+            self.profile_picture = CTkLabel(self, text="", image=ctk_image)
+        else:
+            image_object = Image.open(self.user_manager.default_profile_picture_path)
+            ctk_image=CTkImage(light_image=image_object, dark_image=image_object, size=(120, 120))
+            self.profile_picture=CTkLabel(self,text="", image=ctk_image)        
+        
+        self.profile_picture.pack(pady=10)
+        self.profile_picture.bind("<Enter>",self.on_mouse_entered)
+        self.profile_picture.bind("<Leave>", self.on_mouse_exit)
+        self.profile_picture.bind("<Button-1>", self.on_mouse_clicked)
 
         self.label_background = CTkFrame(self, corner_radius=10,width=135,height=35)
         self.label_background.pack(side="bottom",pady=8)
@@ -32,7 +49,7 @@ class UserLoginIcon(CTkFrame):
         self.label_background.bind("<Leave>", self.on_mouse_exit)
         self.label_background.bind("<Button-1>", self.on_mouse_clicked)
 
-        self.username_label = CTkLabel(self.label_background, text=self.user.username, font=("Roboto",20))
+        self.username_label = CTkLabel(self.label_background, text=self.user.username, font=("Roboto",20), fg_color="transparent")
         self.username_label.pack(expand=True)
 
         self.username_label.bind("<Enter>",self.on_mouse_entered)
@@ -158,12 +175,27 @@ class UserloginMenu(CTkFrame):
         self.user_manager = user_manager
         self.user = user
 
-        self.configure(width=300, height=200)
+        self.configure(width=300, height=450)
         self.pack_propagate(False)
         
-        self.password_label = CTkLabel(self,text=f"Password", font=("Roboto", 30))
+        self.login_to_label=CTkLabel(self,font=("Roboto",40))
+        self.language_manager.register_widget(self.login_to_label, "Login...")
+        self.login_to_label.pack(pady=10)
+
+        image_object=Image.open(self.user.profile_picture_path)
+        ctk_image=CTkImage(light_image=image_object,dark_image=image_object, size=(120,120))
+        self.user_profile = CTkLabel(self, text="", image=ctk_image)
+        self.user_profile.pack(pady=10)
+
+        self.username_label=CTkLabel(self, text=user.username, font=("Roboto",30))
+        self.username_label.pack(pady=10)
+
+        self.username_password_buffer=CTkFrame(self, height=10, fg_color="transparent")
+        self.username_password_buffer.pack()
+
+        self.password_label = CTkLabel(self,text=f"Password", font=("Roboto", 20))
         self.language_manager.register_widget(self.password_label,"Password")
-        self.password_label.pack(pady=20)
+        self.password_label.pack(pady=5)
 
         self.password_entery_text_variable = StringVar()
         self.password_entery_field = CTkEntry(self, width=200, height=30, border_width=0, textvariable=self.password_entery_text_variable)
@@ -185,3 +217,5 @@ class UserloginMenu(CTkFrame):
         self.on_login_button_pressed.send(self, user=self.user, entered_password=self.password_entery_field.get())
     def Cancel_button_pressed(self):
         self.on_cancel_login_button_pressed.send(self)
+    
+    
