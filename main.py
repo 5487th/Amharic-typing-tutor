a70229eeb315f2480c08420cf40bdbec5647849f
5487th <adamasteway268th@gmail.com>
2026-05-01 13:05:41 +0300

    added app icon and tittle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import customtkinter as ctk
 from blinker import signal
+import pathlib
+import ctypes
 
 from scripts.user_manager import UserManager
 from scripts.language_manager import LanguageManager
@@ -13,14 +15,23 @@ root = ctk.CTk()
 User_Manager:UserManager = UserManager()
 language_manager:LanguageManager = LanguageManager()
 
+User_Manager.change_username("unlocked user", "locked user")
 #menus
 user_login_signup_screen=UserloginSignupScreen(root, language_manager, User_Manager)
 
+user_login_signup_screen.open_menu()
+
 root.geometry("1000x600")
-root.minsize(1000,600)
+root.minsize(1000, 600)
+root.title("Amharic typing tutor")
 ctk.set_appearance_mode("light")
 
-user_login_signup_screen.open_menu()
+#icon
+app_id="Amharic.Typing.Tutor.Id"
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
+
+app_icon_path = pathlib.Path(__file__).parent/"assets"/"images"/"amharic typing tutor app icon.ico"
+root.iconbitmap(app_icon_path)
 
 try:
     root.mainloop()
