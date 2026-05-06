d922564d7997b889930800fb51f9b06e632661b4
5487th <adamasteway268th@gmail.com>
2026-05-06 14:11:12 +0300

    add more safty nets and standardize warnings

--- scripts/user_manager.py
+++ scripts/user_manager.py
@@ -5,43 +5,56 @@ from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 import pathlib
 
+
 class User:
     def __init__(self, username, password, profile_picture_path=""):
         self.username = username
         self.password = password
-        self.profile_picture_path=profile_picture_path
+        self.profile_picture_path = profile_picture_path
+
 
 class UserManager:
     def __init__(self):
         self.current_user = None
-        
-        self.usermanager_database_name = "users"
-        
-        self.default_profile_picture_path = pathlib.Path(__file__).parent.parent/'assets'/'images'/'Default profile.png'
 
-        self.user_Data_base_location = pathlib.Path(__file__).parent.parent/'data'/f'{self.usermanager_database_name}.db'
+        self.usermanager_database_name = "users"
+
+        self.default_profile_picture_path = (
+            pathlib.Path(__file__).parent.parent
+            / "assets"
+            / "images"
+            / "Default profile.png"
+        )
+
+        self.user_Data_base_location = (
+            pathlib.Path(__file__).parent.parent
+            / "data"
+            / f"{self.usermanager_database_name}.db"
+        )
 
         self.user_Data_base_location.parent.mkdir(parents=True, exist_ok=True)
 
         self.usermanager_database = sqlite3.connect(self.user_Data_base_location)
         self.usermanager_database_cursor = self.usermanager_database.cursor()
-        
-        self.users_table_name = 'users'
-        self.usermanager_database_cursor.execute(f'''CREATE TABLE IF NOT EXISTS {self.users_table_name}
-                                            (username TEXT PRIMARY KEY, password TEXT, profile_path TEXT)''')
-        
-        self.on_user_created = signal('user_created')
-        self.on_user_deleted = signal('user_deleted')
 
-        self.on_user_logged_in = signal('user_logged_in')
-        self.on_user_logged_out = signal('user_logged_out')
+        self.users_table_name = "users"
+        self.usermanager_database_cursor.execute(
+            f"""CREATE TABLE IF NOT EXISTS {self.users_table_name}
+                                            (username TEXT PRIMARY KEY, password TEXT, profile_path TEXT)"""
+        )
 
-        self.on_user_change_password = signal('user_changed_password')
-        self.on_user_changed_username = signal('user_changed_username')
+        self.on_user_created = signal("user_created")
+        self.on_user_deleted = signal("user_deleted")
 
-    def create_user(self, username, password = "", profile_picture_path="") -> bool:
+        self.on_user_logged_in = signal("user_logged_in")
+        self.on_user_logged_out = signal("user_logged_out")
+
+        self.on_user_change_password = signal("user_changed_password")
+        self.on_user_changed_username = signal("user_changed_username")
+
+    def create_user(self, username, password="", profile_picture_path="") -> bool:
         """
-        creates users, with a hashed password and adds them to the database 
+        creates users, with a hashed password and adds them to the database
 
         Parameters:
         username (string): name of the user to be created, it must be unique.
@@ -51,79 +64,105 @@ class UserManager:
         Returns:
         bool: wether or not the user creation succeded.
         """
-        
-        self.usermanager_database_cursor.execute(f'SELECT (username) FROM {self.users_table_name} Where username = ?', (username,))
-        user_already_exists = self.usermanager_database_cursor.fetchone()
-        
-        if profile_picture_path=="":
-            profile_picture_path=str(self.default_profile_picture_path.resolve())
-        
-        if not username:
-            warnings.warn('called create user function with a "None" username, user not created')
-            return False
-        
-        if user_already_exists:
-             warnings.warn('called a create user with an already existing username, new user not created exists')
-             return False
-        
-        if password == None:
-            warnings.warn('called create user function with a "None" password, user not created')
-            return False
-        
-        ph=PasswordHasher()
-        hashed_password= ph.hash(password)
 
-        self.usermanager_database_cursor.execute(f'INSERT INTO {self.users_table_name} VALUES (?, ?, ?)', (username, hashed_password, profile_picture_path))
+        self.usermanager_database_cursor.execute(
+            f"SELECT (username) FROM {self.users_table_name} Where username = ?",
+            (username,),
+        )
+        user_already_exists = self.usermanager_database_cursor.fetchone()
+
+        if profile_picture_path == "":
+            profile_picture_path = str(self.default_profile_picture_path.resolve())
+
+        if not username:
+            warnings.warn(
+                'called create user function with a "None" username, user not created'
+            )
+            return False
+
+        if user_already_exists:
+            warnings.warn(
+                "called create user with an existing username, new user not created"
+            )
+            return False
+
+        if password == None:
+            warnings.warn(
+                'called create user function with a "None" password, user not created'
+            )
+            return False
+
+        ph = PasswordHasher()
+        hashed_password = ph.hash(password)
+
+        self.usermanager_database_cursor.execute(
+            f"INSERT INTO {self.users_table_name} VALUES (?, ?, ?)",
+            (username, hashed_password, profile_picture_path),
+        )
         self.usermanager_database.commit()
-        self.on_user_created.send(self, user=User(username, password, profile_picture_path))
-          
-    def is_Correct_password(self, hashed, entered):
-        try:
-            ph=PasswordHasher()
-            ph.verify(hashed, entered)
-            return True
-        except VerifyMismatchError:
-            return False      
+        self.on_user_created.send(
+            self, user=User(username, password, profile_picture_path)
+        )
 
     def delete_user(self, username_of_user_to_be_deleted):
         if username_of_user_to_be_deleted:
-            self.usermanager_database_cursor.execute(f"SELECT * FROM {self.users_table_name} WHERE username = ?",(username_of_user_to_be_deleted,))
-            row = self.usermanager_database_cursor.fetchone() 
-            deleted_user=None
+            self.usermanager_database_cursor.execute(
+                f"SELECT * FROM {self.users_table_name} WHERE username = ?",
+                (username_of_user_to_be_deleted,),
+            )
+            row = self.usermanager_database_cursor.fetchone()
+            deleted_user = None
             if row:
                 deleted_user = User(row[0], row[1], row[2])
 
-            self.usermanager_database_cursor.execute(f'DELETE FROM {self.users_table_name} WHERE username = ?', (username_of_user_to_be_deleted,))
+            self.usermanager_database_cursor.execute(
+                f"DELETE FROM {self.users_table_name} WHERE username = ?",
+                (username_of_user_to_be_deleted,),
+            )
             self.usermanager_database.commit()
 
             if deleted_user:
-                self.on_user_deleted.send(self,user=deleted_user)
+                self.on_user_deleted.send(self, user=deleted_user)
             else:
-                warnings.warn('called delete_user function with "None" username, no users were deleted')
-    
+                warnings.warn(
+                    "called delete user function with a 'None' username, no users were deleted"
+                )
+
     def login(self, user_to_login):
         if user_to_login:
             self.current_user = user_to_login
-            self.on_user_logged_in.send(self,user=user_to_login)
+            self.on_user_logged_in.send(self, user=user_to_login)
         else:
-            warnings.warn("login function called with no user set, not loged in")
+            warnings.warn(
+                "login function called with a 'none' user, did not log user in"
+            )
 
     def logout(self):
-        user_to_be_signed_out = self.current_user 
+        user_to_be_signed_out = self.current_user
         self.current_user = None
         self.on_user_logged_out.send(self, signed_out_user=user_to_be_signed_out)
-        
+
     def get_user(self, username):
-        self.usermanager_database_cursor.execute(f'SELECT * FROM {self.users_table_name} WHERE username = ?',(username,))
+        if not username:
+            warnings.warn(
+                "Called get user function with 'None' username, returning None"
+            )
+            return
+
+        self.usermanager_database_cursor.execute(
+            f"SELECT * FROM {self.users_table_name} WHERE username = ?", (username,)
+        )
         row = self.usermanager_database_cursor.fetchone()
         if row:
-            user = User(row[0],row[1],row[2])
+            user = User(row[0], row[1], row[2])
             return user
         else:
             return None
-    
+
     def get_all_users(self):
-        self.usermanager_database_cursor.execute(f"SELECT * FROM {self.users_table_name}")
+        self.usermanager_database_cursor.execute(
+            f"SELECT * FROM {self.users_table_name}"
+        )
         users_tuple_list = self.usermanager_database_cursor.fetchall()
         users_list = []
         for user_tuple in users_tuple_list:
@@ -132,54 +171,96 @@ class UserManager:
         return users_list
 
     def change_user_password(self, username, new_password):
-        self.usermanager_database_cursor.execute(f"SELECT * FROM {self.users_table_name} WHERE username = ?", (username,))
+        if not username:
+            warnings.warn(
+                "called change user functoin with a 'none' username, password not changed"
+            )
+        if not new_password:
+            warnings.warn(
+                "called change user password with a 'None' new password, password not changed"
+            )
+        self.usermanager_database_cursor.execute(
+            f"SELECT * FROM {self.users_table_name} WHERE username = ?", (username,)
+        )
         row = self.usermanager_database_cursor.fetchone()
-        
+
         if not row:
-            warnings.warn(f"no user with username {username}, password not changed")
+            warnings.warn(
+                f"called change user password function with a none existant username, password not changed"
+            )
             return
 
         user = User(row[0], row[1], row[2])
 
-        if user.username:
-            if new_password:
-                ph=PasswordHasher()
-                hashed_password= ph.hash(new_password)
-                self.usermanager_database_cursor.execute(F'UPDATE {self.users_table_name} SET password = ? WHERE username = ?',(hashed_password, username))
-                self.usermanager_database.commit()
-                self.on_user_change_password.send(self, user=User(user.username, new_password, user.profile_picture_path))
-            else:
-                warnings.warn("called change password with no new password, did not change password")
-        else:
-            warnings.warn("called change password with none username, password not changed")
+        ph = PasswordHasher()
+        hashed_password = ph.hash(new_password)
+        self.usermanager_database_cursor.execute(
+            f"UPDATE {self.users_table_name} SET password = ? WHERE username = ?",
+            (hashed_password, username),
+        )
+        self.usermanager_database.commit()
+        self.on_user_change_password.send(
+            self,
+            user=User(user.username, new_password, user.profile_picture_path),
+        )
 
     def change_username(self, old_username, new_username):
         if not old_username:
-            warnings.warn("called change useraname with empty username, did not change username")
+            warnings.warn(
+                "called change useraname function with 'None' old username, did not change username"
+            )
             return
-        
+
         if not new_username:
-            warnings.warn("called change user name with empty new username, username not changed")
+            warnings.warn(
+                "called change username function with 'None' new username, username not changed"
+            )
             return
-        
-        self.usermanager_database_cursor.execute(f"SELECT username FROM {self.users_table_name} WHERE username = ?", (old_username,))
+
+        self.usermanager_database_cursor.execute(
+            f"SELECT username FROM {self.users_table_name} WHERE username = ?",
+            (old_username,),
+        )
         old_username_exists = self.usermanager_database_cursor.fetchone()
 
-        self.usermanager_database_cursor.execute(f"SELECT username FROM {self.users_table_name} WHERE username = ?", (new_username,))
+        self.usermanager_database_cursor.execute(
+            f"SELECT username FROM {self.users_table_name} WHERE username = ?",
+            (new_username,),
+        )
         new_username_exists = self.usermanager_database_cursor.fetchone()
-        
-        if old_username_exists:
-            if not new_username_exists:
-                self.usermanager_database_cursor.execute(f"UPDATE {self.users_table_name} SET username = ? where username = ?",(new_username, old_username))
-                self.usermanager_database.commit()
-                self.on_user_changed_username.send(self, old_username=old_username, new_username=new_username)
-            else:
-                warnings.warn("called change user name with user name that already exist, did not change user name")
-        else:
-            warnings.warn("called change user name with non existant user, did not change Username")
+
+        if not old_username_exists:
+            warnings.warn(
+                "called change user name with non existant user, did not change Username"
+            )
+            return
+
+        if new_username_exists:
+            warnings.warn(
+                "called change user name with user name that already exist, did not change user name"
+            )
+            return
+
+        self.usermanager_database_cursor.execute(
+            f"UPDATE {self.users_table_name} SET username = ? where username = ?",
+            (new_username, old_username),
+        )
+        self.usermanager_database.commit()
+
+        self.on_user_changed_username.send(
+            self, old_username=old_username, new_username=new_username
+        )
 
     def User_exists(self, username):
-        self.usermanager_database_cursor.execute(f"SELECT * FROM {self.users_table_name} WHERE username = ?",(username,))
+        if not username:
+            warnings.warn(
+                "Called user_exists function with 'None' username, returning false"
+            )
+            return False
+
+        self.usermanager_database_cursor.execute(
+            f"SELECT * FROM {self.users_table_name} WHERE username = ?", (username,)
+        )
         row = self.usermanager_database_cursor.fetchone()
 
         if row:
@@ -189,7 +270,17 @@ class UserManager:
 
     def user_has_empty_password(self, username):
         if not username:
-            warnings.warn("called username has empty password fucntion with none username, returning False")
+            warnings.warn(
+                "called username has empty password fucntion with none username, returning False"
+            )
             return False
-        ph=PasswordHasher()
-        return self.is_Correct_password(self.get_user(username).password,"")
+        ph = PasswordHasher()
+        return self.is_Correct_password(self.get_user(username).password, "")
+
+    def is_Correct_password(self, hashed, entered):
+        try:
+            ph = PasswordHasher()
+            ph.verify(hashed, entered)
+            return True
+        except VerifyMismatchError:
+            return False
